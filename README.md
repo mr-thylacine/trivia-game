ebbfebe090329799c0bc694c934317f9de0cc14e
Michelle <ellehcimj@gmail.com>
2018-05-04 16:58:10 -0700

    adds updated readme, image, cleans out js comments

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # trivia-game
-2/15/18 - A fun trivia game (i.e. brutal final exam) using JavaScript and jQuery! I hope you studied! :o3
+thursday, february 15, 2018 - A fun dating game using JavaScript and jQuery!
 
-Don't look right now. It's going through an awkward stage. https://mr-thylacine.github.io/trivia-game/
+### the premise
+
+Date the Wolf is an otome-inspired game. Otome (oh-TOE-may) is a Japanese dating simulator game designed for women or "maidens" and usually follows the viewpoint of the main character and her journey of romancing one of the male love interests in the game. There are usually "good" ends and "bad" ends depending on your in-game choices.
+
+I took interest in this style of storytelling and wanted to create a trivia game that incorporated "right" and "wrong" answers, which would ultimately lead to a "good" or "bad" ending. In this case, the title of this game - Date the Wolf, Not the Others - is a bit of a hint as to what choices you should make to successfully earn a "good" ending.
+
+The theme of this story is also inspired by the characters' actions in the visual novel Doki Doki Literature Club, which I recommend.
+
+### how this game works (code-wise)
+
+As the user reads the prompts, they are given four choices as to how they would like to format their date. Every radio button has a name and value attached to it. The names (q1, q2, etc…) are keys that are stored in an object, and their values are true, false, or null which are used for the grading. The object updates in real-time based on the user's choices. 
+
+When the user submits their choices, the JavaScript goes through the user's choices and counts the amount of correctly-chosen values and the wrongly-chosen values, based on if the value is true or false.
+
+Each correct answer (value = true) means the counter for correct answers increases by one. Each incorrect answer (value = false) means the counter for incorrect answers increases by one. And if the user refuses to answer a question, the answer for that question becomes null and increases the null counter by one. The amount of correct and incorrect answers is shown on the score page.
+
+The timer refreshes every second and is replaced by the new number using jQuery.
+
+### let's play?
+
+Of course! Visit the Wolf here:
+
+https://mr-thylacine.github.io/trivia-game/
+
+![I have a bad feeling about this...](assets/images/wolf1.jpg)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,20 +3,16 @@
 $(document).ready(function() {
 
 // Declares how much time (in seconds) you have left to live. Consult countdown() function to see decrement by 1000 ms (i.e., 1 second each time)
-
 var time = 101;
 
 // stopTime helps us in the functions to... wait for it... stop... time...
-
 var stopTime;
 
 // Declares variables for judging the correct-ness of the userChoice
-
-// var correctAnswers = 0;
-// var incorrectAnswers = 0;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
 
 // Declares a dictionary (answer key) to check the answers against the user's choices
-
 var answers = {
 	q1: null,
 	q2: null,
@@ -25,16 +21,19 @@ var answers = {
 };
 
 // The following 3 variables are used in the checkAnswers() function
-// value - Refers to the values denoted in the radio buttons in HTML
-// question_num - Refers to the first item in value (value[0])
-// answer_index - Refers to the second item in value (value[1])
-
+// value - Refers to values denoted in the radio buttons in HTML
+// question_num - Refers to first item in value (value[0])
+// answer_index - Refers to second item in value (value[1])
 var value;
 var question_num;
 var answer_index;
 
-// countdown() tells us how quickly we should decrement time (in this case, by 1 second each time)
+// These will be used to calculate how many answers the user has correct, incorrect, or unanswered
+var trueCounter = 0;
+var falseCounter = 0;
+var nullCounter = 0;
 
+// countdown() tells us how quickly we should decrement time (in this case, by 1 second each time)
 function countdown() {
 	stopTime = setInterval(decrement, 1000);
 	decrement();
@@ -42,7 +41,6 @@ function countdown() {
 
 // decrement() makes time continue to decrement by 1 second until time = 0, then STOP counting!
 // decrement() updates the in-browser timer to show how many seconds the player has left
-
 function decrement() {
 
 	time--;
@@ -58,13 +56,11 @@ function decrement() {
 }
 
 // stop() uses stopTime to... stop() time *ROLL CREDITS*
-
 function stop() {
 	clearInterval(stopTime);
 }
 
 // checkAnswers() to see if user's answers are correct (or not...)
-
 function checkAnswers() {
 
 	$('.correct-answer').on('click', function() {   
@@ -79,52 +75,11 @@ function checkAnswers() {
 		answers[name] = false;
 		console.log(answers);
 	});
-
-	// $(".correct-answer").on("click", function() {
-	// 	value = $(this).val().split(", ");
-	// 	answers.q1 = true;
-		
-	// 	console.log(answers);
-	// });
-
-	// $(".correct-answer").on("click", function() {
-	// 	value = $(this).val().split(", ");
-	// 	// question_num = value[0];
-	// 	// answer_index =  value[1];
-
-	// 	console.log(`${question_num} is the question_num and ${answer_index} is the answer_index`);
-
-	// 	correctAnswers++;
-	// });
-
-	// $(".incorrect-answer").on("click", function() {
-	// 	value = $(this).val().split(", ");
-	// 	// question_num = value[0];
-	// 	// answer_index = value[1];
-
-	// 	console.log(`${question_num} is the question_num and ${answer_index} is the answer_index`);
-
-	// 	incorrectAnswers++;
-	// });
-
-}
-
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-
-function answerSubmit() {
-	// Counts how many trues and falses there are in the answers object
 }
 
 console.log('answers q1 is ' + answers.q1);
 
-	var trueCounter = 0;
-	var falseCounter = 0;
-	var nullCounter = 0;
-
 function countTrue(object){
-
-
 	$(".outcome-page").on("click", function() {
 	correctAnswers = countTrue(answers);
 
@@ -140,44 +95,13 @@ function countTrue(object){
 		}
 	})
 	return trueCounter;
-});
-
-// $(".outcome-page").on("click", function() {
-// 	correctAnswers = countTrue(summary)
-
-	// console.log(correctAnswers);
-	// console.log(incorrectAnswers);
+	});
 }
 
-	// $(".outcome-page").on("click", function() {
-	// 	for (var i = 0; i < answers.length; i++) {
-	// 		if (Object.is(true, true)) {
-			
-	// 		correctAnswers++;
-	// 		console.log(correctAnswers);
-	// 	}
-	// 	}
-	// });
-
-	// for (var i = 0; i < answers.length; i++) {
-	// 	if (answers[name] == true) {
-	// 		correct++;
-	// 	} else {
-	// 		incorrect++;
-	// 	}
-	// 	console.log(answers);
-	// }
-
-
-	
-
-
 // scorePage() shows you your scores
-
 function scorePage() {
 
 	// Once the user clicks on the Submit button OR if time is up (time = 0), transition to the score page
-
 	$("#ultimate-game-page2").hide();
 
 	var newPage = $("#ultimate-game-page2");
@@ -185,56 +109,23 @@ function scorePage() {
 
 	var badEndnoWolf = $(".no-wolf");
 
-	// if ()
-
 	$(".outcome-page").on("click", function() {
 		stop();
-
 
 		$(".correct-answers").text(trueCounter);
 		$(".incorrect-answers").text(falseCounter);
 		$("#ultimate-game-page").replaceWith(newPage);
 
-			// if (answers.q1 == false) {
-			// 	$("#ultimate-game-page").append(badEndnoWolf);
-			// } else 
-
 		$("#ultimate-game-page2").show();
+
 	});
 
 	if (time === 0) {
 		stop();
-		// $(".correct-answers").text(answers);
-		// $(".incorrect-answers").text(answers);
 		$("#ultimate-game-page").replaceWith(newPage);
 		$("#ultimate-game-page2").show();
 	}
 
-	// $("#return-button").on("click", function() {
-	// 	$("#ultimate-game-page2").replaceWith(originalPage);
-	// 	countdown();
-	// });
-	
-
-	// $(".outcome-page").on("click", function() {
-	// 	stop();
-	// 	$("#ultimate-game-page").html(`You have ${correctAnswers} correct answers and ${incorrectAnswers} incorrect answers.`);
-	// 	if (correctAnswers === 4) {
-	// 		$("#ultimate-game-page").append("<br><br>Perfect ending.");
-	// 	} else {
-	// 		$("#ultimate-game-page").append("<br><br>Bad ending.<br><br>He never let you leave. He devoured you shortly after you went to bed.<br><br>At least you tasted good... right?");
-	// 	}
-	// });
-
-	// if (time === 0) {
-	// 	stop();
-	// 	$("#ultimate-game-page").html(`You have ${correctAnswers} correct answers and ${incorrectAnswers} incorrect answers.`);
-	// 	if (correctAnswers === 4) {
-	// 		$("#ultimate-game-page").append("<br><br>Perfect ending.");
-	// 	} else {
-	// 		$("#ultimate-game-page").append("<br><br>Bad ending.<br><br>He never let you leave. He devoured you shortly after you went to bed.<br><br>At least you tasted good... right?");
-	// 	}
-	// }
 }
 
 countdown();
@@ -243,10 +134,6 @@ checkAnswers();
 
 countTrue(answers);
 
-answerSubmit();
-
 scorePage();
-
-
 
 });
